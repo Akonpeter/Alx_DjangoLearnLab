4412e2e9506d058fcde3befac15269f3615e6cd3
Akon peter <prizylink@gmail.com>
2025-11-27 23:07:43 +0100

    Setting Up a New Django Project with Custom Serializers in Django REST Framework

--- api/models.py
+++ api/models.py
@@ -29,7 +29,7 @@ class Author(models.Model):
 
 
         # Book Serializer
-# ===========================
+
 # Serializes all Book fields.
 # Includes custom validation to 
 # ensure the publication_year is not in the future.
