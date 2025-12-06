dba2148656d0b1f7840eea627e85306e0fb6fb7b
Akon peter <prizylink@gmail.com>
2025-12-06 14:26:17 +0100

    Creating Blog Post Management Features

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
 from .form import RegisterForm
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic import UpdateView
 
 class UserLoginView(LoginView):
     template_name = "blog/login.html"
@@ -35,8 +37,9 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         return self.request.user == post.author
 
 
-from django.contrib.auth.mixins import UserPassesTestMixin
-from django.views.generic import UpdateView
+# 
+# UserPassesTestMixin
+
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
@@ -77,3 +80,54 @@ class PostListView(ListView):
     template_name = "blog/post_list.html"   # HTML to display list
     context_object_name = "posts"
 
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"   # your template
+    context_object_name = "posts"
+    ordering = ['-date_posted']             # latest first
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user   # set logged-in user as author
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author   # only author can edit
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author   # only author can delete
