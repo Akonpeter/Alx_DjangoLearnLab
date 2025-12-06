3843ea4a3bb663923b7ed3b027454423248f5c77
Akon peter <prizylink@gmail.com>
2025-12-06 13:44:06 +0100

    Creating Blog Post Management System

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from . import views  
+
+from . views import( PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    ) 
 from .views import RegisterView, CustomLoginView
 from django.contrib.auth.views import LogoutView
 
@@ -8,5 +14,11 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("profile/", views.profile, name="profile"),
+      path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
     
 ]
