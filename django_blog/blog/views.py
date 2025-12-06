e2a220274e8283d94881ab147c130726c7c271cb
Akon peter <prizylink@gmail.com>
2025-12-06 13:30:30 +0100

    Creating Blog Post Management Features

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -20,3 +20,60 @@ def register_view(request):
         form = RegisterForm()
 
     return render(request, "blog/register.html", {"form": form})
+
+
+from django.views.generic import DeleteView
+from django.urls import reverse_lazy
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("post_list")
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic import UpdateView
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = "blog/post_form.html"
+    fields = ["title", "content"]
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # only author can update
+
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "blog/post_form.html"
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # assign post to logged-in user
+        return super().form_valid(form)
+
+
+from django.views.generic import DetailView
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+
+# blog/views.py
+from django.views.generic import ListView
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"   # HTML to display list
+    context_object_name = "posts"
+
