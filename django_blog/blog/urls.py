f8055771b4ebe713d3b5fbf6708c530f2bdd721f
Akon peter <prizylink@gmail.com>
2025-12-06 12:24:41 +0100

    Implementing The Blog User Auth

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from . import views  
 from .views import RegisterView, CustomLoginView
 from django.contrib.auth.views import LogoutView
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
+    
 ]
