ff8a648780ee9370d970f0fcf8d9729062a69269
Akon peter <prizylink@gmail.com>
2025-12-11 01:41:47 +0100

    Implementing User Follow and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 
 User = get_user_model()
-User.objects.all()
+CustomUser.objects.all()
 
 # Import serializers
 from .serializers import (
