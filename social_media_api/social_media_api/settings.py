040fb80cfd87796e449fb92521ae1c0508568e69
Akon peter <prizylink@gmail.com>
2025-12-15 00:17:59 +0100

    Deploying the Django REST API to Production

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6de2+yuwy69n=wim_am=s=1ii=itdgh+wlratfr^92_#wc=-c4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [www.prizylink.com]
 
 
 # Application definition
@@ -138,9 +138,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ # Prevent Browser XSS Attack
+SECURE_BROWSER_XSS_FILTER = True
+
+# Clickjacking Protection
+X_FRAME_OPTIONS = "DENY"
+
